f5fdf46db4aa188ccff28e3d56a409fec2ea6938
Emirlan Dogdurbaev <emirlandogdurbaev@MacBook-Pro-Emirlan.local>
2023-02-10 00:20:22 +0600

    added new content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
+import { Link } from "react-router-dom";
 import classes from "./Home.module.scss";
 
 const Home = () => {
+      const img1 = "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
   return (
     <main className={classes.Home}>
+      <section className={classes.first}>
+        <h1>Lorem, ipsum. Lorem, ipsum dolor.</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae,
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolorem porro eaque soluta rerum 
+          quis.
+        </p>
+
+        <Link to="contacts">
+            Call
+        </Link>
+      </section>
       <section>
-            <h1>Lorem, ipsum.</h1>
-            
+            <img src={img1} alt="this axaxa"  width={500}/>
       </section>
     </main>
   );
